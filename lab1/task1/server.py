162e676847484eff7db4c97b038b757411be6ff3
Vladyslavg1510 <hlushchukvlad03@gmail.com>
2023-10-08 19:01:15 +0300

    Task 1.2

--- lab1/task1/server.py
+++ lab1/task1/server.py
@@ -1,21 +1,27 @@
 import socket
-import datetime
+import time
 
-# Створення TCP-сервера
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(('127.0.0.1', 12345))  # Підключення до локальної адреси на порту 12345
-server_socket.listen(5)  # Очікування до 5 клієнтів одночасно
+server_socket.bind(('127.0.0.1', 12345))
+server_socket.listen(5)
 
 print("Сервер слухає на порту 12345...")
 
 while True:
-    client_socket, client_address = server_socket.accept()  # Очікування з'єднання
+    client_socket, client_address = server_socket.accept()
     print(f"З'єднано з {client_address}")
 
-    data = client_socket.recv(1024).decode()  # Отримання даних від клієнта (максимум 1024 байти)
+    data = client_socket.recv(1024).decode()
     print(f"Отримано: {data}")
 
-    current_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    response = f"Отримано: {data}\nЧас отримання: {current_time}"
-    client_socket.send(response.encode())  # Надсилання відповіді до клієнта
-    client_socket.close()  # Закриття з'єднання
+    # Затримка на сервері на 5 секунд
+    time.sleep(5)
+
+    # Перевірка, чи відправлені всі дані
+    if data:
+        response = f"Сервер отримав: {data}\nВсі дані успішно відправлені"
+    else:
+        response = "Дані не були відправлені"
+
+    client_socket.send(response.encode())
+    client_socket.close()
